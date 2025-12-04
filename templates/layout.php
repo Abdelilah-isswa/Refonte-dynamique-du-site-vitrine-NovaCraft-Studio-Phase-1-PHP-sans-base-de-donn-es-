d5824c782a32b0ac7080df1755be6388073238c2
Abdelilah Isswa <tomhary66@gmail.com>
2025-12-04 12:42:29 +0100

    add general layout

---
+++ templates/layout.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>DigitalWave Solutions - À propos</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-50 text-gray-800">
+<?php 
+include ("header.php");
+
+?>
+
+<?php
+
+include(dirname(__DIR__).$page );
+
+
+  // require "views/home.php";
+// echo $_SERVER['REQUEST_URI']
+?>
+
+
+<?php 
+include ("footer.php")
+?>
+</body>
+</html>
