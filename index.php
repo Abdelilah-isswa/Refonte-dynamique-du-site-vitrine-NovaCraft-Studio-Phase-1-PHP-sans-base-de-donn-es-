83b0005353631930340e50df4b3766d2735806f3
Abdelilah Isswa <tomhary66@gmail.com>
2025-12-04 12:38:51 +0100

    add index

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+//  echo "<pre>";
+//  var_dump($_SERVER);
+//  echo "<pre>";
+
+// function rout(){
+// if($_SERVER["REQUEST_URI"] == '/services.html'){
+//  return $x = '/services.html';
+// };
+
+// };
+
+// require "templates/layout.php";
+// require "functions.php";
+
+
+require "router.php";
